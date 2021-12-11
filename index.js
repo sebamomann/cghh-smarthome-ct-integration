76c14e16f24a30151fd4e4a09e5a19ed72d8bfd9
sebamomann <github@sebamomann.de>
2021-12-11 14:14:01 +0100

    use axios to send uptime stat

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { execute } = require("./src/churchtools/churchtools-event-cron");
 const { startEventListener } = require("./src/homematic/homematic-event-listener");
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const axios = require('axios');
 
 const CronJob = require('cron').CronJob;
 
@@ -21,12 +21,17 @@ const job = new CronJob(process.env.CRON_DEFINITION, async () => {
 
 job.start();
 
+pingUptime("OK");
 execute();
 startEventListener();
 
 const pingUptime = (message) => {
-    var xmlHttp = new XMLHttpRequest();
     let url = process.env.UPTIME_KUMA_URL + "?msg=" + message + "&ping=";
-    xmlHttp.open("GET", url, false); // false for synchronous request
-    xmlHttp.send(null);
+    axios.get(url)
+        .then((response) => {
+            console.log("Heartbeat send to Uptime");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
