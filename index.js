d1f44f128d7c3a5132488db6ffaa490929e1f298
sebamomann <github@sebamomann.de>
2021-12-11 11:32:12 +0100

    include xmlhttprequest

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { execute } = require("./src/churchtools/churchtools-event-cron");
 const { startEventListener } = require("./src/homematic/homematic-event-listener");
 
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 const CronJob = require('cron').CronJob;
 
 require('dotenv').config();
